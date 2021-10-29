5116f6a8f2b1c99cbb478d5a62c3d89e48e7a06d
asosunoff <al.sosunoff@gmail.com>
2021-10-29 23:00:31 +0300

    fix render

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { createBrowserHistory } from "history";
 
 import { configureStore } from "./app/redux";
-import { PeopleDetail } from "./feature/people-detail";
-import { NotFound } from "./feature/not-found";
-import { People } from "./feature/people";
+import { Router } from "./app/router";
 
 export const history = createBrowserHistory();
 
@@ -17,15 +14,7 @@ const store = configureStore(history);
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" exact component={People} />
-
-        <Route path="/people" exact component={People} />
-
-        <Route path="/people-detail" exact component={PeopleDetail} />
-
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Router />
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
