9008c2a11e5b6473a8e9ffbb44a9fbb064430455
asosunoff <al.sosunoff@gmail.com>
2021-10-29 23:34:31 +0300

    fix rootSaga

--- src/app/redux/sagas/index.ts
+++ src/app/redux/sagas/index.ts
@@ -1,51 +1,8 @@
-import { spawn, call, all, take, fork, put } from "redux-saga/effects";
-import { LOCATION_CHANGE, LocationChangeAction } from "connected-react-router";
-import { SWAPIService } from "../../services/SWAPIService";
-import { PagesType } from "../../interfaces.ts/pages-type";
-import { Person } from "../../interfaces.ts/person";
-import {
-  ActionPeaoleFailure,
-  ActionPeopleRequest,
-  ActionPeopleSuccess,
-} from "../../../feature/people/reducer";
-import { Path } from "../../router/path-constant";
-
-function* loadPeopleList() {
-  yield put<ActionPeopleRequest>({
-    type: "REQUEST",
-  });
-
-  try {
-    const pageData: PagesType<Person> = yield call(SWAPIService.getPeople);
-
-    yield put<ActionPeopleSuccess>({
-      type: "SUCCESS",
-      payload: pageData,
-    });
-  } catch (error: any) {
-    yield put<ActionPeaoleFailure>({
-      type: "FAILURE",
-      payload: error,
-    });
-  }
-}
-
-function* watchRoute() {
-  while (true) {
-    const action: LocationChangeAction = yield take(LOCATION_CHANGE);
-
-    if (
-      [Path.Main, Path.People].includes(
-        action.payload.location.pathname as Path
-      )
-    ) {
-      yield fork(loadPeopleList);
-    }
-  }
-}
+import { spawn, call, all } from "redux-saga/effects";
+import { watchRoutePeople } from "../../../feature/people/sagas/watchRoutePeople";
 
 export function* rootSaga() {
-  const sagas = [watchRoute];
+  const sagas = [watchRoutePeople];
 
   const retrySagas = sagas.map((saga) =>
     spawn(function* () {
