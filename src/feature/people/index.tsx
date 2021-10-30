2c3ebcf6a064e3eb86deaaf6addbc9de0e6e9dd4
asosunoff <al.sosunoff@gmail.com>
2021-10-30 22:38:37 +0300

    fi page people

--- src/feature/people/index.tsx
+++ src/feature/people/index.tsx
@@ -1,35 +1,20 @@
 import React from "react";
-import { Table } from "antd";
-import { useSelector } from "react-redux";
-import { v4 } from "uuid";
-import { useDispatchPeopleRequest } from "./dispatch/use-dispatch-people-request";
-import { selectorPeople } from "./selectors/selector-people";
+import { Row, Col } from "antd";
+import { SearchPeopleByName } from "./components/search-people-by-name";
+import { TablePeople } from "./components/table-people";
 
 export interface PeopleProps {}
 
 export const People: React.FC<PeopleProps> = () => {
-  const { pageData, page, loading } = useSelector(selectorPeople);
-
-  const { dispatchPepleRequest } = useDispatchPeopleRequest();
-
   return (
-    <Table
-      loading={loading}
-      rowKey={() => v4()}
-      columns={[
-        { dataIndex: "name", title: "Name" },
-        { dataIndex: "birth_year", title: "Birth Year" },
-      ]}
-      dataSource={pageData?.results ?? []}
-      pagination={{
-        position: ["bottomLeft"],
-        current: page,
-        pageSize: 10,
-        total: pageData?.count ?? 0,
-      }}
-      onChange={(page) => {
-        dispatchPepleRequest(page?.current ?? 1, "");
-      }}
-    />
+    <Row gutter={[15, 15]}>
+      <Col span={10}>
+        <SearchPeopleByName />
+      </Col>
+
+      <Col span={24}>
+        <TablePeople />
+      </Col>
+    </Row>
   );
 };
