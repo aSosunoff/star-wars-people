ffeed5e41d34f99dd82b1c9b0b464892943790b8
asosunoff <al.sosunoff@gmail.com>
2021-10-30 23:20:21 +0300

    fix search

--- src/feature/people/components/search-people-by-name.tsx
+++ src/feature/people/components/search-people-by-name.tsx
@@ -1,19 +1,32 @@
 import { Input } from "antd";
 import { useSelector } from "react-redux";
+import { useForm, InitialForm } from "@asosunoff/react_use_form";
 import { useDispatchPeopleRequest } from "../dispatch/use-dispatch-people-request";
 import { selectorPeople } from "../selectors/selector-people";
+import { useEffect } from "react";
 
 export const SearchPeopleByName: React.FC = () => {
   const { page, loading, search } = useSelector(selectorPeople);
 
   const { dispatchPepleRequest } = useDispatchPeopleRequest();
 
+  const { handlers, initialFormHandler } = useForm();
+
+  useEffect(() => {
+    initialFormHandler({
+      name: { value: search },
+    });
+  }, [initialFormHandler, search]);
+
   return (
     <Input.Search
       placeholder="write name"
       loading={loading}
       allowClear
-      value={search}
+      value={handlers.name?.value}
+      onChange={(ev) => {
+        handlers.name?.onChange(ev.target.value);
+      }}
       onSearch={(value) => {
         dispatchPepleRequest(page, value);
       }}
