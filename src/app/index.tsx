2d6e3c0993319aa768a2d4d4c22ea39d11a150b6
asosunoff <al.sosunoff@gmail.com>
2021-10-30 13:30:57 +0300

    added app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Layout } from "antd";
+import { Router } from "./router";
 
 export const App: React.FC = () => {
-  return <>App</>;
+  return (
+    <Layout style={{ minHeight: "100vh", maxHeight: "100vh" }}>
+      <Layout>
+        <Layout.Content>
+          <Router />
+        </Layout.Content>
+      </Layout>
+    </Layout>
+  );
 };
