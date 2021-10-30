d563121a1b1e9079526504115a8d9c13e8ee2195
asosunoff <al.sosunoff@gmail.com>
2021-10-30 14:18:56 +0300

    added table people

--- src/feature/people/index.tsx
+++ src/feature/people/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Table } from "antd";
+import { useSelector } from "react-redux";
+import { v4 } from "uuid";
+import { useDispatchPeopleRequest } from "./dispatch/use-dispatch-people-request";
+import { selectorPeople } from "./selectors/selector-people";
 
 export interface PeopleProps {}
 
 export const People: React.FC<PeopleProps> = () => {
-  return <>People</>;
+  const { pageData, page, loading } = useSelector(selectorPeople);
+
+  const { dispatchPepleRequest } = useDispatchPeopleRequest();
+
+  return (
+    <Table
+      loading={loading}
+      rowKey={() => v4()}
+      columns={[
+        { dataIndex: "name", title: "Name" },
+        { dataIndex: "birth_year", title: "Birth Year" },
+      ]}
+      dataSource={pageData?.results ?? []}
+      pagination={{
+        position: ["bottomLeft"],
+        current: page,
+        pageSize: 10,
+        total: pageData?.count ?? 0,
+      }}
+      onChange={(page) => {
+        dispatchPepleRequest(page?.current ?? 1, "");
+      }}
+    />
+  );
 };
