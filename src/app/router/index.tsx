c93d7101c5b774d4780245e9227c76bdcefd61c3
asosunoff <al.sosunoff@gmail.com>
2021-10-31 15:32:41 +0300

    added router

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { PeopleDetail } from "../../feature/people-detail";
-import { NotFound } from "../../feature/not-found";
-import { People } from "../../feature/people";
-import { Path } from "./path-constant";
-import { Planets } from "../../feature/planets";
+import { routesConfig } from "./routes-config";
 
 export interface RouterProps {}
 
 export const Router: React.FC<RouterProps> = () => (
   <Switch>
-    <Route path={Path.Main} exact component={People} />
-
-    <Route path={Path.People} exact component={People} />
-
-    <Route path={Path.PeopleDetail} exact component={PeopleDetail} />
-
-    <Route path={Path.Planets} exact component={Planets} />
-
-    <Route path="*" component={NotFound} />
+    {routesConfig.map((route) => (
+      <Route key={route.path as string} {...route} />
+    ))}
   </Switch>
 );
