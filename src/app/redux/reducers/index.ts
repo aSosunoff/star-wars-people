1844de0ccd89b3009dfa3cef7daef06a801b3c25
asosunoff <al.sosunoff@gmail.com>
2021-11-01 01:33:09 +0300

    fix reducer

--- src/app/redux/reducers/index.ts
+++ src/app/redux/reducers/index.ts
@@ -4,10 +4,12 @@ import { History } from "history";
 
 import { peopleReducer } from "../../../feature/people/reducer";
 import { planetsReducer } from "../../../feature/planets/reducer";
+import { peopleDetailReducer } from "../../../feature/people-detail/reducer";
 
 export const createRootReducer = (history: History) =>
   combineReducers({
     people: peopleReducer,
+    peopleDetail: peopleDetailReducer,
     planets: planetsReducer,
     router: connectRouter(history),
   });
