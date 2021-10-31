e6aedba004560a22e9d94b5daa474a7c761c5e33
asosunoff <al.sosunoff@gmail.com>
2021-10-31 15:32:58 +0300

    added router config

---
+++ src/app/router/routes-config.tsx
@@ -0,0 +1,34 @@
+import { RouteProps } from "react-router-dom";
+import { NotFound } from "../../feature/not-found";
+import { People } from "../../feature/people";
+import { PeopleDetail } from "../../feature/people-detail";
+import { Planets } from "../../feature/planets";
+import { Path } from "./path-constant";
+
+export const routesConfig: RouteProps[] = [
+  {
+    path: Path.Main,
+    exact: true,
+    component: People,
+  },
+  {
+    path: Path.People,
+    exact: true,
+    component: People,
+  },
+  {
+    path: Path.PeopleDetail,
+    exact: true,
+    component: PeopleDetail,
+  },
+  {
+    path: Path.Planets,
+    exact: true,
+    component: Planets,
+  },
+  {
+    path: "*",
+    exact: true,
+    component: NotFound,
+  },
+];
