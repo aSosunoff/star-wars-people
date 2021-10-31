1a1c23f81c568b58dd5116b55c3b722422e57236
asosunoff <al.sosunoff@gmail.com>
2021-10-31 14:11:17 +0300

    added redicer

---
+++ src/feature/planets/reducer.ts
@@ -0,0 +1,59 @@
+import { Reducer } from "redux";
+
+import { CreateReducer, Handler } from "../../app/redux/reducers/helpers";
+import { PagesType } from "../../app/interfaces.ts/pages-type";
+import {
+  ActionPlanetsFailure,
+  ActionPlanets,
+  ActionPlanetsRequest,
+  ActionPlanetsSuccess,
+  PLANETS_REQUEST,
+  PLANETS_FAILURE,
+  PLANETS_SUCCESS,
+} from "./action";
+import { Planet } from "../../app/interfaces.ts/planet";
+
+type StatePlanets = {
+  pageData: PagesType<Planet> | null;
+  page: number;
+  pageSize: number;
+  search: string;
+  loading: boolean;
+  error: {
+    detail: string;
+  } | null;
+};
+
+const initialState: StatePlanets = {
+  pageData: null,
+  page: 1,
+  pageSize: 10,
+  search: "",
+  loading: false,
+  error: null,
+};
+
+const handlers: Handler<StatePlanets, ActionPlanets> = {
+  [PLANETS_REQUEST]: (state, action) => ({
+    ...state,
+    ...action.payload,
+    loading: true,
+    error: null,
+  }),
+  [PLANETS_FAILURE]: (state, action) => ({
+    ...state,
+    loading: false,
+    error: action.payload,
+  }),
+  [PLANETS_SUCCESS]: (state, action) => ({
+    ...state,
+    loading: false,
+    pageData: action.payload,
+  }),
+  DEFAULT: (state) => state,
+};
+
+export const planetsReducer: Reducer<
+  StatePlanets,
+  ActionPlanetsRequest | ActionPlanetsSuccess | ActionPlanetsFailure
+> = (state = initialState, action) => CreateReducer(handlers, state, action);
