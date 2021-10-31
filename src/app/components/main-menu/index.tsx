41208f3ca8c373ff1896d4cdc81b60e3477b8a17
asosunoff <al.sosunoff@gmail.com>
2021-11-01 00:06:50 +0300

    added meny

---
+++ src/app/components/main-menu/index.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Menu } from "antd";
+import { matchPath } from "react-router";
+import { Link } from "react-router-dom";
+import { routesConfig } from "../../router/routes-config";
+import { selectorPathName } from "../../router/selectors-router";
+import { Path } from "../../router/path-constant";
+
+export const MainMenu: React.FC = () => {
+  const pathname = useSelector(selectorPathName);
+
+  const defaultSelectedKey = useMemo(() => {
+    const config = routesConfig.find((config) => matchPath(pathname, config));
+
+    return config ? (config.path as string) : "";
+  }, [pathname]);
+
+  return (
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      defaultSelectedKeys={[defaultSelectedKey]}
+    >
+      <Menu.Item key={Path.People}>
+        <Link to={Path.People}>People</Link>
+      </Menu.Item>
+      <Menu.Item key={Path.Planets}>
+        <Link to={Path.Planets}>Planets</Link>
+      </Menu.Item>
+    </Menu>
+  );
+};
