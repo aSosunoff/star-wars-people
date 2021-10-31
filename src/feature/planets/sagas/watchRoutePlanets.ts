46f9b4542a97ccaf4e717b4ecb2e3e81d97d47fc
asosunoff <al.sosunoff@gmail.com>
2021-11-01 00:30:16 +0300

    fix watch router planets

--- src/feature/planets/sagas/watchRoutePlanets.ts
+++ src/feature/planets/sagas/watchRoutePlanets.ts
@@ -1,15 +1,20 @@
-import { selectorPlanets } from "./../selectors/selector-planets";
 import { take, select, put } from "redux-saga/effects";
 import { LOCATION_CHANGE, LocationChangeAction } from "connected-react-router";
+import { matchPath } from "react-router-dom";
+
+import { selectorPlanets } from "./../selectors/selector-planets";
 import { Path } from "../../../app/router/path-constant";
 import { StateRoot } from "../../../app/redux/reducers";
 import { ActionPlanetsRequest, PLANETS_REQUEST } from "../action";
+import { getRouteConfig } from "../../../app/router/routes-config";
 
 export function* watchRoutePlanets() {
   while (true) {
     const action: LocationChangeAction = yield take(LOCATION_CHANGE);
 
-    if ([Path.Planets].includes(action.payload.location.pathname as Path)) {
+    const planets = getRouteConfig(Path.Planets);
+
+    if (planets && matchPath(action.payload.location.pathname, planets)) {
       const { page, search }: StateRoot["planets"] = yield select(
         selectorPlanets
       );
