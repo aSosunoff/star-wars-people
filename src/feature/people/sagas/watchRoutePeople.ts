e6df2388e07f501994417ee81db04fd6480577fd
asosunoff <al.sosunoff@gmail.com>
2021-11-01 00:30:07 +0300

    fix watchRouterPeople

--- src/feature/people/sagas/watchRoutePeople.ts
+++ src/feature/people/sagas/watchRoutePeople.ts
@@ -1,18 +1,24 @@
-import { selectorPeople } from "./../selectors/selector-people";
 import { take, select, put } from "redux-saga/effects";
 import { LOCATION_CHANGE, LocationChangeAction } from "connected-react-router";
+import { matchPath } from "react-router-dom";
+
+import { selectorPeople } from "./../selectors/selector-people";
 import { Path } from "../../../app/router/path-constant";
 import { StateRoot } from "../../../app/redux/reducers";
 import { ActionPeopleRequest, PEOPLE_REQUEST } from "../action";
+import { getRouteConfig } from "../../../app/router/routes-config";
 
 export function* watchRoutePeople() {
   while (true) {
     const action: LocationChangeAction = yield take(LOCATION_CHANGE);
 
+    const main = getRouteConfig(Path.Main);
+
+    const people = getRouteConfig(Path.People);
+
     if (
-      [Path.Main, Path.People].includes(
-        action.payload.location.pathname as Path
-      )
+      (people && matchPath(action.payload.location.pathname, people)) ||
+      (main && matchPath(action.payload.location.pathname, main))
     ) {
       const { page, search }: StateRoot["people"] = yield select(
         selectorPeople
