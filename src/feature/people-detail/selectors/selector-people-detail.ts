c149694bbfb179d9374d3805f81e2cc6a7cbc649
asosunoff <al.sosunoff@gmail.com>
2021-11-02 00:36:42 +0300

    fix selector

--- src/feature/people-detail/selectors/selector-people-detail.ts
+++ src/feature/people-detail/selectors/selector-people-detail.ts
@@ -1,3 +1,11 @@
 import { StateRoot } from "../../../app/redux/reducers";
 
-export const selectorPeopleDetail = (state: StateRoot) => state.peopleDetail;
+export const selectorPeopleDetail = (state: StateRoot) => {
+  return {
+    ...state.peopleDetail,
+    person:
+      state.peopleDetail.id && state.peopleDetail.persons[state.peopleDetail.id]
+        ? state.peopleDetail.persons[state.peopleDetail.id].person
+        : null,
+  };
+};
