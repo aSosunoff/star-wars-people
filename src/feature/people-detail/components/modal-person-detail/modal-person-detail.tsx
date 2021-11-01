186dedba1097736b2f1735dc403c67ed2015e227
asosunoff <al.sosunoff@gmail.com>
2021-11-01 23:01:46 +0300

    added modal person detail

---
+++ src/feature/people-detail/components/modal-person-detail/modal-person-detail.tsx
@@ -0,0 +1,33 @@
+import { Modal, ModalProps } from "antd";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatchPeopleDetailRequest } from "../../dispatch/use-dispatch-people-detail-request";
+import { selectorPeopleDetail } from "../../selectors/selector-people-detail";
+import { PeopleDetail } from "../people-detail";
+
+interface ModalPersonDetailProps
+  extends Pick<ModalProps, "visible" | "onCancel"> {
+  id: string;
+}
+
+export const ModalPersonDetail: React.FC<ModalPersonDetailProps> = ({
+  id,
+  visible,
+  onCancel,
+}) => {
+  const { pageData, loading } = useSelector(selectorPeopleDetail);
+
+  const { dispatchPeopleDetailRequest } = useDispatchPeopleDetailRequest();
+
+  useEffect(() => {
+    if (visible) {
+      dispatchPeopleDetailRequest(id);
+    }
+  }, [dispatchPeopleDetailRequest, id, visible]);
+
+  return (
+    <Modal width="800px" visible={visible} onCancel={onCancel} footer={null}>
+      <PeopleDetail person={pageData} loading={loading} />
+    </Modal>
+  );
+};
