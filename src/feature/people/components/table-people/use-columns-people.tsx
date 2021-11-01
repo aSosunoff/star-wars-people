f8be01de9a882399081d60e66d560c3c620bd202
asosunoff <al.sosunoff@gmail.com>
2021-11-01 23:25:46 +0300

    fix

---
+++ src/feature/people/components/table-people/use-columns-people.tsx
@@ -0,0 +1,61 @@
+import { EyeOutlined, UserOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { ColumnsType } from "antd/lib/table";
+import moment from "moment";
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
+import { Person } from "../../../../app/interfaces/person";
+import { Path } from "../../../../app/router/path-constant";
+import { setParameter } from "../../../../app/router/set-parameter";
+import { getIdUserFromUrl } from "../../../../app/utils/getIdUserFromUrl";
+
+const COLUMNS: ColumnsType<Person> = [
+  { dataIndex: "name", title: "Name" },
+  { dataIndex: "birth_year", title: "Birth Year" },
+  {
+    dataIndex: "edited",
+    title: "Edited",
+    render: (edited) => moment(edited).format("DD.MM.YYYY HH:MM:SS"),
+  },
+  { dataIndex: "eye_color", title: "Eye Color" },
+  { dataIndex: "gender", title: "Gender" },
+  { dataIndex: "hair_color", title: "Hair Color" },
+];
+
+export const useColumnsPeople = (
+  showPesronDetail: (idPrson: string) => void
+) => {
+  const columns = useMemo<ColumnsType<Person>>(
+    () => [
+      ...COLUMNS,
+      {
+        dataIndex: "url",
+        render: (url: Person["url"]) => (
+          <Button
+            shape="circle"
+            icon={<EyeOutlined />}
+            onClick={() => {
+              showPesronDetail(getIdUserFromUrl(url));
+            }}
+            type="ghost"
+          />
+        ),
+      },
+      {
+        dataIndex: "url",
+        render: (url: Person["url"]) => (
+          <Link
+            to={setParameter(Path.PeopleDetail, {
+              ":id": getIdUserFromUrl(url),
+            })}
+          >
+            <Button shape="circle" icon={<UserOutlined />} type="primary" />
+          </Link>
+        ),
+      },
+    ],
+    [showPesronDetail]
+  );
+
+  return columns;
+};
