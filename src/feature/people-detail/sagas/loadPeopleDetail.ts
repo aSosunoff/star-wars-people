6c62983f0e5b966e9a135f4624b59860386507aa
asosunoff <al.sosunoff@gmail.com>
2021-11-02 00:36:31 +0300

    fix load person

--- src/feature/people-detail/sagas/loadPeopleDetail.ts
+++ src/feature/people-detail/sagas/loadPeopleDetail.ts
@@ -1,6 +1,8 @@
+import moment from "moment";
 import { call, put } from "redux-saga/effects";
 
 import { Person } from "../../../app/interfaces/person";
+import { StateRoot } from "../../../app/redux/reducers";
 import { SWAPIService } from "../../../app/services/SWAPIService";
 import {
   ActionPeopleDetailFailure,
@@ -9,17 +11,38 @@ import {
   PEOPLE_DETAIL_FAILURE,
   PEOPLE_DETAIL_SUCCESS,
 } from "../action";
+import { getPerson } from "./getPerson";
 
 export function* loadPeopleDetail({
   payload: { id },
 }: ActionPeopleDetailRequest) {
   try {
-    const pageData: Person = yield call(SWAPIService.getPeopleDetail, id);
+    const person: StateRoot["peopleDetail"]["persons"][any] | null = yield call(
+      getPerson,
+      id
+    );
 
-    yield put<ActionPeopleDetailSuccess>({
-      type: PEOPLE_DETAIL_SUCCESS,
-      payload: pageData,
-    });
+    if (!person) {
+      const data: Person = yield call(SWAPIService.getPeopleDetail, id);
+
+      yield put<ActionPeopleDetailSuccess>({
+        type: PEOPLE_DETAIL_SUCCESS,
+        payload: {
+          id,
+          person: data,
+          expired: moment().add(2, "m").toDate().getTime(),
+        },
+      });
+    } else {
+      yield put<ActionPeopleDetailSuccess>({
+        type: PEOPLE_DETAIL_SUCCESS,
+        payload: {
+          id,
+          person: person.person,
+          expired: person.expired,
+        },
+      });
+    }
   } catch (error: any) {
     yield put<ActionPeopleDetailFailure>({
       type: PEOPLE_DETAIL_FAILURE,
