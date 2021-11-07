8ad71fb32d4eb567ff81766aa2c3f6bee9cba99b
asosunoff <al.sosunoff@gmail.com>
2021-11-07 14:06:30 +0300

    added saga to root saga

--- src/app/redux/sagas/index.ts
+++ src/app/redux/sagas/index.ts
@@ -1,10 +1,11 @@
 import { spawn, call, all } from "redux-saga/effects";
 import { peopleDetailSaga } from "../../../feature/people-detail/sagas";
 import { peopleSaga } from "../../../feature/people/sagas";
+import { planetDetailSaga } from "../../../feature/planet-detail/sagas";
 import { planetsSaga } from "../../../feature/planets/sagas";
 
 export function* rootSaga() {
-  const sagas = [peopleSaga, planetsSaga, peopleDetailSaga];
+  const sagas = [peopleSaga, planetsSaga, peopleDetailSaga, planetDetailSaga];
 
   const retrySagas = sagas.map((saga) =>
     spawn(function* () {
