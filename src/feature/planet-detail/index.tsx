8764d375e4057a9d21b15d06a49e98d8ac815cd9
asosunoff <al.sosunoff@gmail.com>
2021-11-07 14:06:49 +0300

    fix page

--- src/feature/planet-detail/index.tsx
+++ src/feature/planet-detail/index.tsx
@@ -6,14 +6,14 @@ import { useSelector } from "react-redux";
 import { selectorPlanetDetail } from "./selectors/selector-planet-detail";
 import { PlanetDetail } from "./components/planet-detail";
 
-export interface PeopleDetailPageProps extends Pick<CardProps, "loading"> {
+export interface PlanetDetailPageProps extends Pick<CardProps, "loading"> {
   person: Person | null;
 }
 
-export const PeopleDetailPage: React.FC<PeopleDetailPageProps> = () => {
+export const PlanetDetailPage: React.FC<PlanetDetailPageProps> = () => {
   const { planet, loading } = useSelector(selectorPlanetDetail);
 
   return (
-    <PlanetDetail title="Person detail" planet={planet} loading={loading} />
+    <PlanetDetail title="Planet detail" planet={planet} loading={loading} />
   );
 };
