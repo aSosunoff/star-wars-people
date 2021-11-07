4f2a2eb062e9d563a97e6f09edb770db064b0bca
asosunoff <al.sosunoff@gmail.com>
2021-11-07 13:21:36 +0300

    fix action

--- src/feature/people-detail/action.ts
+++ src/feature/people-detail/action.ts
@@ -1,22 +1,6 @@
 import { Person } from "../../app/interfaces/person";
 import { CreateAction } from "../../app/redux/reducers/helpers";
 
-export type ActionPeopleDetail = {
-  PEOPLE_DETAIL_REQUEST: {
-    payload: {
-      id: string;
-    };
-  };
-  PEOPLE_DETAIL_SUCCESS: {
-    payload: {
-      id: string;
-      person: Person;
-      expired: number;
-    };
-  };
-  PEOPLE_DETAIL_FAILURE: { payload: any };
-};
-
 export const PEOPLE_DETAIL_REQUEST = "PEOPLE_DETAIL_REQUEST";
 export type dispatchPeopleDetailRequest = typeof PEOPLE_DETAIL_REQUEST;
 export type ActionPeopleDetailRequest = CreateAction<
@@ -37,3 +21,19 @@ export type ActionPeopleDetailFailure = CreateAction<
   ActionPeopleDetail,
   dispatchPeopleDetailFailure
 >;
+
+export type ActionPeopleDetail = {
+  [PEOPLE_DETAIL_REQUEST]: {
+    payload: {
+      id: string;
+    };
+  };
+  [PEOPLE_DETAIL_SUCCESS]: {
+    payload: {
+      id: string;
+      person: Person;
+      expired: number;
+    };
+  };
+  [PEOPLE_DETAIL_FAILURE]: { payload: any };
+};
