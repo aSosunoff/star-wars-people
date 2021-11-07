9a0f259609a04f097cf79bba7f50c83c50dc8721
asosunoff <al.sosunoff@gmail.com>
2021-11-08 00:18:22 +0300

    added test watch router people

---
+++ src/feature/people/sagas/watchRoutePeople.test.ts
@@ -0,0 +1,34 @@
+import { take, put } from "redux-saga/effects";
+import { LOCATION_CHANGE, LocationChangeAction } from "connected-react-router";
+import { watchRoutePeople } from "./watchRoutePeople";
+import { ActionPeopleRequest, PEOPLE_REQUEST } from "../action";
+
+describe("watchRoutePeople df", () => {
+  const page = 1;
+  const search = "";
+  let gen: ReturnType<typeof watchRoutePeople>;
+
+  beforeEach(() => {
+    gen = watchRoutePeople();
+  });
+
+  it("try", () => {
+    expect(gen.next().value).toEqual(take(LOCATION_CHANGE));
+
+    const action: LocationChangeAction = {
+      type: LOCATION_CHANGE,
+      payload: {
+        location: {
+          pathname: "/people",
+        },
+      },
+    } as LocationChangeAction;
+
+    expect(gen.next(action).value).toEqual(
+      put<ActionPeopleRequest>({
+        type: PEOPLE_REQUEST,
+        payload: { page, search },
+      })
+    );
+  });
+});
