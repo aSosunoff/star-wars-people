12690fcebd3248732122dafdf910b97f04faacc0
asosunoff <al.sosunoff@gmail.com>
2021-11-07 13:03:30 +0300

    fix

--- src/feature/planets/sagas/watchRoutePlanets.ts
+++ src/feature/planets/sagas/watchRoutePlanets.ts
@@ -1,10 +1,10 @@
-import { take, select, put } from "redux-saga/effects";
+import { take, /* select, */ put } from "redux-saga/effects";
 import { LOCATION_CHANGE, LocationChangeAction } from "connected-react-router";
 import { matchPath } from "react-router-dom";
 
-import { selectorPlanets } from "./../selectors/selector-planets";
+/* import { selectorPlanets } from "./../selectors/selector-planets";
+import { StateRoot } from "../../../app/redux/reducers"; */
 import { Path } from "../../../app/router/path-constant";
-import { StateRoot } from "../../../app/redux/reducers";
 import { ActionPlanetsRequest, PLANETS_REQUEST } from "../action";
 import { getRouteConfig } from "../../../app/router/routes-config";
 
@@ -15,13 +15,13 @@ export function* watchRoutePlanets() {
     const planets = getRouteConfig(Path.Planets);
 
     if (planets && matchPath(action.payload.location.pathname, planets)) {
-      const { page, search }: StateRoot["planets"] = yield select(
+      /* const { page, search }: StateRoot["planets"] = yield select(
         selectorPlanets
-      );
+      ); */
 
       yield put<ActionPlanetsRequest>({
         type: PLANETS_REQUEST,
-        payload: { page, search },
+        payload: { page: 1, search: "" },
       });
     }
   }
