ef1457763db6299574d5902551d1f040bae755e8
asosunoff <al.sosunoff@gmail.com>
2021-11-09 19:19:24 +0300

    fix

--- cypress/integration/star-wars-people/index.spec.ts
+++ cypress/integration/star-wars-people/index.spec.ts
@@ -19,8 +19,14 @@ const LukeSkywalker = {
     "https://swapi.dev/api/films/6/",
   ],
   species: [],
-  vehicles: ["https://swapi.dev/api/vehicles/14/", "https://swapi.dev/api/vehicles/30/"],
-  starships: ["https://swapi.dev/api/starships/12/", "https://swapi.dev/api/starships/22/"],
+  vehicles: [
+    "https://swapi.dev/api/vehicles/14/",
+    "https://swapi.dev/api/vehicles/30/",
+  ],
+  starships: [
+    "https://swapi.dev/api/starships/12/",
+    "https://swapi.dev/api/starships/22/",
+  ],
   created: "2014-12-09T13:50:51.644000Z",
   edited: "2014-12-20T21:17:56.891000Z",
   url: "https://swapi.dev/api/people/1/",
@@ -34,7 +40,9 @@ describe("star-wars-people", () => {
 
     cy.get("@searchWrapper").find(".ant-input[type='text']").as("seatchInput");
 
-    cy.get("@searchWrapper").find(".ant-input-search-button").as("seatchButton");
+    cy.get("@searchWrapper")
+      .find(".ant-input-search-button")
+      .as("seatchButton");
 
     cy.get(".ant-breadcrumb > span").as("breadcrumbs");
   });
@@ -48,7 +56,9 @@ describe("star-wars-people", () => {
   it("should fetch to people api and return 10 items", () => {
     cy.intercept("GET", "https://swapi.dev/api/people/").as("getPeople");
     cy.visit(baseUrl);
-    cy.wait("@getPeople").its("response.body.results").should("have.length", 10);
+    cy.wait("@getPeople")
+      .its("response.body.results")
+      .should("have.length", 10);
   });
 
   it("should fetch to people", () => {
@@ -64,9 +74,15 @@ describe("star-wars-people", () => {
   it("should be breadcrumb home/main before start page", () => {
     cy.get("@breadcrumbs").should("have.length", 2);
 
-    cy.get("@breadcrumbs").first().find(".ant-breadcrumb-link").should("have.text", "Home");
+    cy.get("@breadcrumbs")
+      .first()
+      .find(".ant-breadcrumb-link")
+      .should("have.text", "Home");
 
-    cy.get("@breadcrumbs").last().find(".ant-breadcrumb-link").should("have.text", "Main");
+    cy.get("@breadcrumbs")
+      .last()
+      .find(".ant-breadcrumb-link")
+      .should("have.text", "Main");
   });
 
   it("should be search with button", () => {
@@ -107,7 +123,9 @@ describe("star-wars-people", () => {
       "display: none;"
     );
 
-    cy.get("[data-cypress=modal-detail-person]").find(".ant-modal-close").click();
+    cy.get("[data-cypress=modal-detail-person]")
+      .find(".ant-modal-close")
+      .click();
 
     cy.get("[data-cypress=modal-detail-person] .ant-modal-wrap").should(
       "have.attr",
@@ -123,7 +141,9 @@ describe("star-wars-people", () => {
 
     cy.visit(baseUrl);
 
-    cy.get("table tbody tr:nth-child(1) td [data-cypress=open-page-person-detail]").click();
+    cy.get(
+      "table tbody tr:nth-child(1) td [data-cypress=open-page-person-detail]"
+    ).click();
 
     cy.url().should("include", `people-detail/1`);
 
@@ -133,12 +153,15 @@ describe("star-wars-people", () => {
   it("should be caused 1 time fetch for a few identical api", () => {
     const spyRequest = cy.spy().as("spyRequest");
 
-    cy.intercept({ method: "GET", url: "https://swapi.dev/api/people/" }, (req) => {
-      spyRequest();
-      return {
-        results: [LukeSkywalker],
-      };
-    }).as("getPeople");
+    cy.intercept(
+      { method: "GET", url: "https://swapi.dev/api/people/" },
+      (req) => {
+        spyRequest();
+        return {
+          results: [LukeSkywalker],
+        };
+      }
+    ).as("getPeople");
 
     cy.visit(baseUrl);
 
@@ -146,7 +169,9 @@ describe("star-wars-people", () => {
       ".ant-layout table tbody tr:nth-child(1) td [data-cypress=open-modal-person-detail]"
     ).click();
 
-    cy.get("[data-cypress=modal-detail-person]").find(".ant-modal-close").click();
+    cy.get("[data-cypress=modal-detail-person]")
+      .find(".ant-modal-close")
+      .click();
 
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     /* cy.wait(5000); */
