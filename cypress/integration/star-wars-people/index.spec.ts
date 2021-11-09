579942e7a40f7028e8dacee9c567de83e68a5647
asosunoff <al.sosunoff@gmail.com>
2021-11-09 18:29:29 +0300

    Fix

--- cypress/integration/star-wars-people/index.spec.ts
+++ cypress/integration/star-wars-people/index.spec.ts
@@ -2,6 +2,30 @@
 
 const baseUrl = "http://localhost:3001";
 
+const LukeSkywalker = {
+  name: "Luke Skywalker",
+  height: "172",
+  mass: "77",
+  hair_color: "blond",
+  skin_color: "fair",
+  eye_color: "blue",
+  birth_year: "19BBY",
+  gender: "male",
+  homeworld: "https://swapi.dev/api/planets/1/",
+  films: [
+    "https://swapi.dev/api/films/1/",
+    "https://swapi.dev/api/films/2/",
+    "https://swapi.dev/api/films/3/",
+    "https://swapi.dev/api/films/6/",
+  ],
+  species: [],
+  vehicles: ["https://swapi.dev/api/vehicles/14/", "https://swapi.dev/api/vehicles/30/"],
+  starships: ["https://swapi.dev/api/starships/12/", "https://swapi.dev/api/starships/22/"],
+  created: "2014-12-09T13:50:51.644000Z",
+  edited: "2014-12-20T21:17:56.891000Z",
+  url: "https://swapi.dev/api/people/1/",
+};
+
 describe("star-wars-people", () => {
   beforeEach(() => {
     cy.visit(baseUrl);
@@ -29,31 +53,7 @@ describe("star-wars-people", () => {
 
   it("should fetch to people", () => {
     cy.intercept("GET", "https://swapi.dev/api/people/", {
-      results: [
-        {
-          name: "Luke Skywalker",
-          height: "172",
-          mass: "77",
-          hair_color: "blond",
-          skin_color: "fair",
-          eye_color: "blue",
-          birth_year: "19BBY",
-          gender: "male",
-          homeworld: "https://swapi.dev/api/planets/1/",
-          films: [
-            "https://swapi.dev/api/films/1/",
-            "https://swapi.dev/api/films/2/",
-            "https://swapi.dev/api/films/3/",
-            "https://swapi.dev/api/films/6/",
-          ],
-          species: [],
-          vehicles: ["https://swapi.dev/api/vehicles/14/", "https://swapi.dev/api/vehicles/30/"],
-          starships: ["https://swapi.dev/api/starships/12/", "https://swapi.dev/api/starships/22/"],
-          created: "2014-12-09T13:50:51.644000Z",
-          edited: "2014-12-20T21:17:56.891000Z",
-          url: "https://swapi.dev/api/people/1/",
-        },
-      ],
+      results: [LukeSkywalker],
     }).as("getPeople");
 
     cy.visit(baseUrl);
@@ -61,7 +61,7 @@ describe("star-wars-people", () => {
     cy.wait("@getPeople").its("response.body.results").should("have.length", 1);
   });
 
-  /* it("should be breadcrumb home/main before start page", () => {
+  it("should be breadcrumb home/main before start page", () => {
     cy.get("@breadcrumbs").should("have.length", 2);
 
     cy.get("@breadcrumbs").first().find(".ant-breadcrumb-link").should("have.text", "Home");
@@ -114,5 +114,47 @@ describe("star-wars-people", () => {
       "style",
       "display: none;"
     );
-  }); */
+  });
+
+  it("should be go to new page of detail person and return main page", () => {
+    cy.intercept("GET", "https://swapi.dev/api/people/", {
+      results: [LukeSkywalker],
+    }).as("getPeople");
+
+    cy.visit(baseUrl);
+
+    cy.get("table tbody tr:nth-child(1) td [data-cypress=open-page-person-detail]").click();
+
+    cy.url().should("include", `people-detail/1`);
+
+    cy.get(".ant-menu-root").contains("People").click();
+  });
+
+  it("should be caused 1 time fetch for a few identical api", () => {
+    const spyRequest = cy.spy().as("spyRequest");
+
+    cy.intercept({ method: "GET", url: "https://swapi.dev/api/people/" }, (req) => {
+      spyRequest();
+      return {
+        results: [LukeSkywalker],
+      };
+    }).as("getPeople");
+
+    cy.visit(baseUrl);
+
+    cy.get(
+      ".ant-layout table tbody tr:nth-child(1) td [data-cypress=open-modal-person-detail]"
+    ).click();
+
+    cy.get("[data-cypress=modal-detail-person]").find(".ant-modal-close").click();
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    /* cy.wait(5000); */
+
+    cy.get(
+      ".ant-layout table tbody tr:nth-child(1) td [data-cypress=open-modal-person-detail]"
+    ).click();
+
+    cy.get("@spyRequest").its("callCount").should("eq", 1);
+  });
 });
